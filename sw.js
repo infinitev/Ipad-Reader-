5b7f89ded8f7d3c91d596173d287ab7987532a66
infinitev <75050126+infinitev@users.noreply.github.com>
2026-02-12 09:33:35 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,11 @@
+// Self-destruct: clear caches and unregister
+self.addEventListener('install', function() { self.skipWaiting(); });
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(names) {
+      return Promise.all(names.map(function(n) { return caches.delete(n); }));
+    }).then(function() {
+      return self.registration.unregister();
+    })
+  );
+});
